2aae4e40b766544cd385d421fc03a188b7ccd416
silkehenkes <silkehenkes@gmail.com>
2025-10-17 16:03:58 +0200

    Finally adding the parameter class

---
+++ Param.py
@@ -0,0 +1,65 @@
+# Parameters for pebble game dynamics, includes default values
+# Location data: not hard-coded, will change every time
+# param.folder - location of data
+# param.step - assuming it's part of an experimental run / simulation: numerical information about sub-set
+
+# Information about the type of data
+# param.setup - type of data: simulation, experiment_square, experiment_annulus, lattice
+import csv
+
+class Paramreader:
+    def __init__(self,paramfile):
+        # store in a dictionary
+        self.params = {}
+        with open(paramfile, newline='') as csvfile:
+            paramreader = csv.reader(csvfile, delimiter=',')
+            ## Automate dictionary construction, e.g. file like this
+            # datatype, experiment_square,
+            # mu ,0.3,
+            # periodic , False,
+            # has_angles , False,
+            # density ,1060, # bulk material density kg/m^3
+            # stiffness ,6490, # prefactor of the stiffness coefficient k_n  \frac{\pi}{6} E h  
+            # pixel_convert,0.00027,# radius conversion factor from pixel to m.
+            # thickness ,0.0031,# thickness of the disks 
+            # width ,0.02,  # boundary width
+            # prefix1 , DSC      ,# Prefix on the image file names 
+            # prefix2 , _solved_Tracks_,
+            # experiment , True,
+            # position_error,0.1,#not a real value yet
+            # force_error,0.1, #not a real value yet
+            # pebble_game_m, 3, # pebble game (m,n): m value
+            # pebble_game_n, 2, # pebble game (m,n): n value
+            for row in paramreader:
+                lhs = row[0].strip()
+                rhs = row[1]
+                # it's either a int, float or a boolean
+                try:
+                    rhs = eval(row[1])
+                except:
+                    rhs = row[1].strip()
+                self.params[lhs] = rhs
+        print(self.params)
+        ## postprocess particular items:
+        # Convert main periodic string 'True' / 'False' and make x and y inherit
+        # Expect this to be 'checkxy' if x and y are different
+        if not self.params['periodic']:
+            # if not explicitly set, x and y parts inherit from full one
+            self.params['periodicx']=False
+            self.params['periodicy']=False
+        if self.params['periodic']:
+            # if not explicitly set, x and y parts inherit from full one
+            self.params['periodicx']=True
+            self.params['periodicy']=True
+        # Consistency: Reset the global periodic if x and y are different
+        if self.params['periodicx'] != self.params['periodicy']:
+            self.params['periodic']='checkxy'
+       
+
+
+
+
+
+
+
+
---
+++ Runscript_square_Prajima.py
@@ -0,0 +1,108 @@
+import random
+import sys, os, glob
+
+
+# Location of the library files: insert into python path
+#pebblepath='/home/sh18581/Documents/Friction/RigidLibrary/'
+#sys.path.insert(1,pebblepath)
+
+# Load pebble game and dynamical matrix libraries
+import Param as PR
+import Configuration as CF
+import Pebbles as PB
+import Hessian as HS
+import Analysis as AN
+import Holes as HO
+
+import matplotlib.pyplot as plt
+
+# Location of data to analyse
+folder = './DataPrajima/'
+paramfile = 'params.csv'
+posfile='particle_positions.txt'
+confile='Adjacency_list.txt'
+# We will still assume that these are part of a sequence
+step=1
+
+ThisPar = PR.Paramreader(folder+paramfile)
+# ThisPar.params
+print(ThisPar.params)
+
+
+# ThisConf = CF.Configuration(foldername,'experiment_square', mu, u)
+# ########## Reading in the configuration and the next step
+# # read in a specific step, careful with format of label
+# numlabel0 = inilabel+dlabel*u
+# numlabel = "%05d" %numlabel0
+# print(numlabel)
+# #def ReadExpdata(self,numlabel,scale=False):
+# ThisConf.ReadExpdataSquare(numlabel)
+# ThisConf.AddBoundaryContactsSquare()
+# # also read in the next data at this point: dlabel further along in the numbering 
+# # Revise at a later stage.
+# #def ReadExpdataNext(self,numlabel,scale=False):
+# #numlabel2 =  "%05d" %(numlabel0+dlabel)
+# #ThisConf.ReadExpdataNext(numlabel2)
+# #ThisConf.AddNextBoundaryContacts()
+
+
+# ########## Setting up and playing the pebble game
+# # def __init__(self,conf, game10,game20,modifier='nothing',verbose0=False):
+# ThisPebble = PB.Pebbles(ThisConf,3,3,'nothing',False)
+# # play game
+# ThisPebble.play_game()
+# # compute rigid clusters
+# cidx, clusterall, clusterallBonds, clusteridx, BigCluster=ThisPebble.rigid_cluster()
+
+
+# ########### Setting up the dynamical matrix and getting eigenmodes
+# # This itself does very little, just creates an empty Hessian class
+# # __init__(self,conf0):
+# ThisHessian = HS.Hessian(ThisConf)
+
+# ########## Have a look at some analysis functions of the rigid clusters
+# #ddef __init__(self,conf0,pebbles0,hessian0,tiling0='skip',fgiven0=0.001,verbose0=False):
+# ThisAnalysis=AN.Analysis(ThisConf,ThisPebble,ThisHessian,'skip',0.01,False)
+# # stress statistics
+# zav,nm,pres,fxbal,fybal,torbal,mobin,mohist,sxx,syy,sxy,syx=ThisAnalysis.getStressStat()
+# # cluster statistics
+# frac,fracmax,lenx,leny=ThisAnalysis.clusterStatistics()
+# #def plotStresses(self,plotCir,plotVel,plotCon,plotF,plotStress,**kwargs):
+# fig1 = ThisAnalysis.plotStresses(True,False,False,True,False)
+# #def plotPebbles(self,plotCir,plotPeb,plotPebCon,plotClus,plotOver,**kwargs):
+# #ThisAnalysis.plotPebbles(True,True,True,False,False)
+# fig2 = ThisAnalysis.plotPebbles(True,True,True,False,False)
+# fig3 = ThisAnalysis.plotPebbles(True,True,False,True,False)
+
+
+######### continuing with the Hessian now 
+# constructing the matrix
+#  makeHessian(self,frictional,recomputeFnor,stabilise,verbose=False):
+#ThisHessian.makeHessian(True,False,0,False)
+# diagonalising the matrix
+# def getModes(self,debug=False):
+#ThisHessian.getModes(False)
+
+##### a couple of checks on the modes (optional)
+#plotModes(self,usepts):
+#usepts=[3*ThisConf.N-5,3*ThisConf.N-4,3*ThisConf.N-3,3*ThisConf.N-2,3*ThisConf.N-1]
+#ThisHessian.plotModes(usepts)
+#plotZeroModes(self,thresh=2e-8,simple=True):
+#ThisHessian.plotZeroModes()
+
+############ Now look for the cross-correlations
+# what is rigid according to modes with a given threshold:
+#fig3 = ThisAnalysis.ModeClusters('translations',2e-4)
+# how this cross-correlates to rigidy according to pebbles:
+#P_eig_if_pebble,P_pebble_if_eig,fig5 = ThisAnalysis.RigidModesCorrelate(2e-4)
+# These are the conditional probabilities of being rigid by mode while being rigid by pebble and the reverse
+#print (P_eig_if_pebble,P_pebble_if_eig)
+# if there is a next data set
+# Needs revision in any case, don't use for now
+#if ThisConf.Nnext>0:
+#    P_disp_if_pebble,P_pebble_if_disp, fig6 = ThisAnalysis.RigidDisplacementsCorrelate(2e-4)
+#    # Conditional probabilities of being rigid by displacement while being rigid by pebble
+#    print (P_disp_if_pebble,P_pebble_if_disp)
+#    # D2_min, needs assessment
+#    fig7 = ThisAnalysis.DisplacementCorrelateD2min(True)
+                                              
